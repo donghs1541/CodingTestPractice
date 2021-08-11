a16e8fdd7ffc3f6b24e1f6ea3ec74f3462587213
donghs1541 <63491788+donghs1541@users.noreply.github.com>
2021-08-11 19:56:06 +0900

    124 나라의 숫자 풀이
    Programmers 124 나라의 숫자 Level 2
    https://donghs1541.tistory.com/20

--- Programmers/Level 2/124 나라의 숫자.py
+++ Programmers/Level 2/124 나라의 숫자.py
@@ -1,15 +1,16 @@
 def solution(n):
 
     answer = ''
-    while n > 0:
-        n -= 1
-        value = n%3
+
+    while n > 0: #n이 없어질때까지
+        n -= 1  # n 1차감
+        value = n%3 # 3진법이므로 n을 3으로 나눈 나머지를 계산
         if value == 0:
             answer = '1' +answer
         elif value == 1:
             answer = '2' +answer
         elif value == 2:
             answer = '4' +answer
-        n //= 3
+        n //= 3 #자릿수 내림
 
     return answer
